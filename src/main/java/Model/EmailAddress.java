b9b830153d91fc947f1b727c260e5185ed9f189c
nathan.seville <nathan.seville@hotmail.ch>
2019-04-10 10:01:05 +0200

    Email Validation

--- src/main/java/Model/EmailAddress.java
+++ src/main/java/Model/EmailAddress.java
@@ -1,10 +1,16 @@
 package Model;
 
+import java.util.regex.Pattern;
+
 public class EmailAddress {
     private String email;
 
-    public EmailAddress(String email) {
-        this.email = email;
+    public EmailAddress(String email) throws EmailBadFormat {
+        if(isValidEmailAddress(email)) {
+            this.email = email;
+        } else {
+            throw new EmailBadFormat("Email format isn't conformed");
+        }
     }
 
     public String getEmail() {
@@ -20,5 +26,18 @@ public class EmailAddress {
     @Override
     public String toString() {
         return email;
+    }
+
+    private boolean isValidEmailAddress(String email) {
+        // OWASP email regex validation
+        Pattern validEmailAddress = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
+
+        return validEmailAddress.matcher(email).matches();
+    }
+
+    public static class EmailBadFormat extends Exception{
+        public EmailBadFormat(String message) {
+            super(message);
+        }
     }
 }
